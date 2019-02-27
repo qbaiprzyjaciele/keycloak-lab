3ddc40b1e8300c577fd9e7cb9651d79cdaf69146
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2019-02-27 22:35:58 +0100

    app scaffold

---
+++ keycloak-lab-app/src/main/java/io/github/qbaiprzyjaciele/keycloaklab/keycloaklab/KeycloakLabApplication.java
@@ -0,0 +1,13 @@
+package io.github.qbaiprzyjaciele.keycloaklab.keycloaklab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakLabApplication.class, args);
+	}
+
+}
